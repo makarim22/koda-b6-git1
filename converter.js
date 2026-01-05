2831ed41ba6e441f9e5cedb0a6380c920d084488
makarim muhammad <jakamakarim@gmail.com>
2026-01-05 21:30:19 +0700

    edit main function

--- converter.js
+++ converter.js
@@ -3,19 +3,58 @@ import Reamur from "./reamur.js";
 import Kelvin from "./kelvin.js";
 import Fahrenheit from "./fahrenheit.js";
 
-function main(temp){
-    Celsius.set(temp);
-    console.log(Celsius.toReamur(), Celsius.toKelvin(), Celsius.toFahrenheit());
+import readline from 'readline';
 
-    Reamur.set(temp);
-    console.log(Reamur.toCelsius(), Reamur.toKelvin(), Reamur.toFahrenheit());
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-    Kelvin.set(temp);
-    console.log(Kelvin.toCelsius(), Kelvin.toReamur(), Kelvin.toFahrenheit());
-
-
-   Fahrenheit.set(temp);
-   console.log(Fahrenheit.toCelsius(), Fahrenheit.toReamur(), Fahrenheit.toKelvin());
+function main() {
+    console.log("kalkulator suhu");
+    console.log("pilih pilihan suhu berikut:");
+    console.log("1. Celsius");
+    console.log("2. Reamur");
+    console.log("3. Kelvin");
+    console.log("4. Fahrenheit");
+    
+    rl.question("masukkan angka saja, 1 untuk kelvin, 2 untuk reamur dst... (1-4): ", (scaleChoice) => {
+        rl.question("masukkan besaran temperatur: ", (temp) => {
+        
+            const temperature = parseFloat(temp);
+            
+            switch (scaleChoice) {
+                case '1': 
+                    Celsius.set(temperature);
+                    console.log(`${Celsius.value}°C = ${Celsius.toReamur()}°R`);
+                    console.log(`${Celsius.value}°C = ${Celsius.toKelvin()}K`);
+                    console.log(`${Celsius.value}°C = ${Celsius.toFahrenheit()}°F`);
+                    break;
+                case '2': 
+                    Reamur.set(temperature);
+                    console.log(`${Reamur.value}°R = ${Reamur.toCelsius()}°C`);
+                    console.log(`${Reamur.value}°R = ${Reamur.toKelvin()}K`);
+                    console.log(`${Reamur.value}°R = ${Reamur.toFahrenheit()}°F`);
+                    break;
+                case '3': 
+                    Kelvin.set(temperature);
+                    console.log(`${Kelvin.value}K = ${Kelvin.toCelsius()}°C`);
+                    console.log(`${Kelvin.value}K = ${Kelvin.toReamur()}°R`);
+                    console.log(`${Kelvin.value}K = ${Kelvin.toFahrenheit()}°F`);
+                    break;
+                case '4': 
+                    Fahrenheit.set(temperature);
+                    console.log(`${Fahrenheit.value}°F = ${Fahrenheit.toCelsius()}°C`);
+                    console.log(`${Fahrenheit.value}°F = ${Fahrenheit.toReamur()}°R`);
+                    console.log(`${Fahrenheit.value}°F = ${Fahrenheit.toKelvin()}K`);
+                    break;
+                default:
+                    console.log("Pilih rentang 1 sampai 4.");
+                    break;
+            }
+            rl.close(); 
+        });
+    });
 }
 
-main(100);
+main();
